f032fb082791ce9de7d8ddae69025533affc2c9f
aymenkhazri <aymenkhazri6@gmail.com>
2020-12-27 23:23:46 +0100

    Initial commit

---
+++ app/src/main/java/com/dsi31g4/corona_tracker/Landing.java
@@ -0,0 +1,28 @@
+package com.dsi31g4.corona_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+public class Landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing);
+
+        Button btnStart =findViewById(R.id.start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
